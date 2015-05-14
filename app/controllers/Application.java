d6240c5b6b5396da23c35049d94bb35cb69d494e
Martin Goffan <martin.goffan@me.com>
2015-05-13 21:31:21 -0300

    first steps

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,24 @@
 package controllers;
 
+import java.util.List;
+
 import play.*;
 import play.libs.Json;
 import play.mvc.*;
+import scala.collection.immutable.Seq;
 import views.html.*;
+import models.MenuItem;
+import play.api.libs.json.*;
+import play.data.Form;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Application extends Controller {
 
+	
+	
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
@@ -21,7 +31,42 @@ public class Application extends Controller {
     	result.put("err", 0);
     	result.put("msg", "API is running");
     	
+    	ObjectMapper mapper = new ObjectMapper();
+    	
+    	
+    	List<MenuItem> items = MenuItem.find().all();
+    	
+    	
+    	ArrayNode array = mapper.valueToTree(items);
+    	
+    	result.putArray("result").addAll(array);
+    	
     	return ok(result);
+    }
+    
+    public static Result createItem() {
+    	
+    	Form<MenuItem> menuItemForm = Form.form(MenuItem.class);
+    	Form<MenuItem> filledForm = menuItemForm.bindFromRequest(request());
+    	
+    	ObjectNode result = Json.newObject();
+    	
+    	if(filledForm.hasErrors()) {
+    		
+    		result.put("err", 3);
+    		result.put("msg", filledForm.errorsAsJson());
+    		
+    		return badRequest(result);
+    	} else {
+    		MenuItem.create(filledForm.get());
+    		
+    		result.put("err", 0);
+    		result.put("msg", "Operacion OK");
+    		result.put("result", Json.toJson(filledForm.get()));
+    		
+    		return ok(result);  
+    	}
+    	
     }
 
 }
--- app/models/MenuItem.java
+++ app/models/MenuItem.java
@@ -2,24 +2,65 @@ package models;
 
 import java.util.Set;
 
-@MongoEntity("menuItems")
-public class MenuItem extends MongoModel {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-	private String _id;
+import com.avaje.ebean.Ebean;
+
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+@Entity
+@Table(name = "menuItems")
+public class MenuItem extends Model {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Long id;
+	@Required
 	private String name;
-	private Set<String> pic;
 	private String description;
 	
-	public String getId() {
-		return this._id;
+	/*
+	 * Getters & Setters
+	 */
+	
+	public Long getId() {
+		return this.id;
 	}
+	
 	
 	public String getName() {
 		return this.name;
 	}
 	
-	public String getPic() {
-		
+	public void setName(String name) {
+		this.name = name;
 	}
 	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public static Finder<Long, MenuItem> find() {
+		return new Finder<Long, MenuItem>(Long.class, MenuItem.class);
+	}
+	
+	public static void create(MenuItem item) {
+		item.save();
+	}
+	
+	@Override
+	public String toString() {
+		return this.id + " " + this.name;
+	}
 }
